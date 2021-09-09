41b8d15daf95852658c328deac8a18390c46b814
Andrew Fitzpatrick <andrew.fitzpatrick@clicktravel.com>
2021-09-09 16:11:39 +0100

    Added model and model tests

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,22 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    """Recipe object"""
+    name = models.CharField(max_length=31)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient object"""
+    name = models.CharField(max_length=31)
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
